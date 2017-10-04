13528b27ce499134bd7f84b539490d57e52ca2eb
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2017-10-04 17:46:21 +0200

    Refactor the OIDC error mapping.

--- modules/core/src/main/scala/ch/epfl/bluebrain/nexus/iam/core/auth/DownstreamAuthClient.scala
+++ modules/core/src/main/scala/ch/epfl/bluebrain/nexus/iam/core/auth/DownstreamAuthClient.scala
@@ -3,8 +3,8 @@ package ch.epfl.bluebrain.nexus.iam.core.auth
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import cats.MonadError
+import cats.syntax.functor._
 import ch.epfl.bluebrain.nexus.commons.http.HttpClient.UntypedHttpClient
-import ch.epfl.bluebrain.nexus.commons.http.UnexpectedUnsuccessfulHttpResponse
 import journal.Logger
 
 /**
@@ -24,24 +24,19 @@ class DownstreamAuthClient[F[_]](implicit F: MonadError[F, Throwable], cl: Untyp
     * @return the response in an ''F'' context
     */
   def forward(request: HttpRequest): F[HttpResponse] = {
-    F.map(cl(request)) { response =>
-      if (response.status.isSuccess()) {
-        response
-      } else {
-        logAndMapStatusCode(request, response)
-      }
-    }
-  }
-
-  private def logAndMapStatusCode(request: HttpRequest, response: HttpResponse): HttpResponse = {
-    log.warn(s"""Unexpected status code from OIDC provider ${response.status} ${request.uri}""")
-    response.status match {
-      case BadRequest                       => HttpResponse(InternalServerError)
+    def mapFailed(resp: HttpResponse): HttpResponse = resp.status match {
       case Unauthorized                     => HttpResponse(Unauthorized)
       case Forbidden                        => HttpResponse(Forbidden)
       case InternalServerError | BadGateway => HttpResponse(BadGateway)
       case GatewayTimeout                   => HttpResponse(GatewayTimeout)
-      case _                                => throw UnexpectedUnsuccessfulHttpResponse(response)
+      case _                                => HttpResponse(InternalServerError)
+    }
+
+    cl(request) map {
+      case resp if resp.status.isSuccess() => resp
+      case resp =>
+        log.warn(s"""Unexpected status code from OIDC provider ${resp.status} ${request.uri}""")
+        mapFailed(resp)
     }
   }
 }
