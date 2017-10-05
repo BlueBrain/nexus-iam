3b2feec7fe4ceee66921d8d71234996f803e713d
Wojtek Wajerowicz <wwajerowicz@users.noreply.github.com>
2017-10-05 14:08:00 +0200

    Fix bug in mapping responses from OIDC provider (#25)
    * Fix bug in mapping responses from OIDC provider
    
    * Refactor the OIDC error mapping.

--- modules/core/src/test/scala/ch/epfl/bluebrain/nexus/iam/core/auth/DownstreamAuthClientSpec.scala
+++ modules/core/src/test/scala/ch/epfl/bluebrain/nexus/iam/core/auth/DownstreamAuthClientSpec.scala
@@ -2,18 +2,17 @@ package ch.epfl.bluebrain.nexus.iam.core.auth
 
 import java.util.UUID
 
+import akka.http.scaladsl.client.RequestBuilding._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Location
-import akka.http.scaladsl.client.RequestBuilding._
-import ch.epfl.bluebrain.nexus.commons.http.HttpClient.UntypedHttpClient
-import ch.epfl.bluebrain.nexus.commons.http.UnexpectedUnsuccessfulHttpResponse
-import org.scalatest.mockito.MockitoSugar
-import org.scalatest.prop.TableDrivenPropertyChecks
-import org.scalatest.{BeforeAndAfter, Matchers, WordSpecLike}
 import cats.instances.future._
+import ch.epfl.bluebrain.nexus.commons.http.HttpClient.UntypedHttpClient
 import org.mockito.Mockito
 import org.mockito.Mockito.when
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{BeforeAndAfter, Matchers, WordSpecLike}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -36,39 +35,35 @@ class DownstreamAuthClientSpec
   "DownstreamAuthClient" should {
     val successResponses = Table(
       "response",
-      Future.successful(HttpResponse(StatusCodes.OK, entity = """{"response": "OK"}""")),
-      Future.successful(HttpResponse(StatusCodes.Found).addHeader(Location(Uri("https://example.com/redirect"))))
+      HttpResponse(StatusCodes.OK, entity = """{"response": "OK"}"""),
+      HttpResponse(StatusCodes.Found).addHeader(Location(Uri("https://example.com/redirect")))
     )
 
     "forward requests and return successful responses" in {
       forAll(successResponses) { expectedResp =>
         val request = Get(s"http://example.com/${UUID.randomUUID().toString}")
         when(cl.apply(request))
-          .thenReturn(expectedResp)
+          .thenReturn(Future.successful(expectedResp))
 
-        client.forward(request) shouldBe expectedResp
+        client.forward(request).futureValue shouldBe expectedResp
       }
     }
 
     val errorResponses = Table(
       ("downstream response", "expected status code"),
-      (Future.failed(UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.BadRequest))),
-       StatusCodes.InternalServerError),
-      (Future.failed(UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.Forbidden))), StatusCodes.Forbidden),
-      (Future.failed(UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.Unauthorized))),
-       StatusCodes.Unauthorized),
-      (Future.failed(UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.InternalServerError))),
-       StatusCodes.BadGateway),
-      (Future.failed(UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.BadGateway))), StatusCodes.BadGateway),
-      (Future.failed(UnexpectedUnsuccessfulHttpResponse(HttpResponse(StatusCodes.GatewayTimeout))),
-       StatusCodes.GatewayTimeout)
+      (HttpResponse(StatusCodes.BadRequest), StatusCodes.InternalServerError),
+      (HttpResponse(StatusCodes.Forbidden), StatusCodes.Forbidden),
+      (HttpResponse(StatusCodes.Unauthorized), StatusCodes.Unauthorized),
+      (HttpResponse(StatusCodes.InternalServerError), StatusCodes.BadGateway),
+      (HttpResponse(StatusCodes.BadGateway), StatusCodes.BadGateway),
+      (HttpResponse(StatusCodes.GatewayTimeout), StatusCodes.GatewayTimeout)
     )
 
     "map error responses to correct status codes" in {
       forAll(errorResponses) { (errorResponse, expectedErrorCode) =>
         val request = Get(s"http://example.com/${UUID.randomUUID().toString}")
         when(cl.apply(request))
-          .thenReturn(errorResponse)
+          .thenReturn(Future.successful(errorResponse))
         client.forward(request).futureValue.status shouldBe expectedErrorCode
       }
     }
