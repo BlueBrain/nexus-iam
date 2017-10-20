527debd2788ed7375c5695212e2c042cf2d17676
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2017-10-20 13:40:32 +0200

    Add @id field to identities.

--- modules/service/src/main/scala/ch/epfl/bluebrain/nexus/iam/service/queue/KafkaEncoder.scala
+++ modules/service/src/main/scala/ch/epfl/bluebrain/nexus/iam/service/queue/KafkaEncoder.scala
@@ -1,6 +1,7 @@
 package ch.epfl.bluebrain.nexus.iam.service.queue
 
 import ch.epfl.bluebrain.nexus.iam.core.acls._
+import ch.epfl.bluebrain.nexus.iam.core.identity.Identity
 import io.circe.generic.extras.Configuration
 import io.circe.generic.extras.auto._
 import io.circe.syntax._
@@ -21,6 +22,21 @@ trait KafkaEncoder {
   protected implicit val metaEncoder: Encoder[Meta] = Encoder.encodeJson.contramap { _ =>
     Json.Null
   }
+
+  import Identity._
+  protected implicit val identityEncoderWithId: Encoder[Identity] = identityEncoder.mapJson { json: Json =>
+    val idOption = for {
+      origin  <- fieldAsString(json, "origin")
+      subject <- fieldAsString(json, "subject") orElse fieldAsString(json, "group")
+    } yield s"$origin/$subject"
+    idOption match {
+      case Some(id) => json.asObject.map(_.add("@id", Json.fromString(id)).asJson).getOrElse(json)
+      case None     => json
+    }
+  }
+
+  private def fieldAsString(json: Json, field: String): Option[String] =
+    json.asObject.flatMap(_(field)).flatMap(_.asString)
 
 }
 // $COVERAGE-ON$
