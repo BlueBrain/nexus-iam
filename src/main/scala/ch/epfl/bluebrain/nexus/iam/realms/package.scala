d5580fb4ffbe11d2000cffaf9d13d36d4ae3b7bd
Bogdan Roman <bogdan.roman@epfl.ch>
2018-12-06 13:55:00 +0100

    Added realm states

---
+++ src/main/scala/ch/epfl/bluebrain/nexus/iam/realms/package.scala
@@ -0,0 +1,32 @@
+package ch.epfl.bluebrain.nexus.iam
+
+import ch.epfl.bluebrain.nexus.iam.config.Vocabulary.nxv
+import ch.epfl.bluebrain.nexus.iam.types.{Permission, ResourceF, ResourceMetadata}
+import ch.epfl.bluebrain.nexus.rdf.Iri.AbsoluteIri
+import ch.epfl.bluebrain.nexus.sourcing.Aggregate
+
+package object realms {
+
+  /* Type annotations specific to realms */
+  type Event     = RealmEvent
+  type State     = RealmState
+  type Command   = RealmCommand
+  type Rejection = RealmRejection
+  type Agg[F[_]] = Aggregate[F, String, Event, State, Command, Rejection]
+
+  type EventOrRejection = Either[Rejection, Event]
+  type MetaOrRejection  = Either[Rejection, ResourceMetadata]
+
+  type Resource    = ResourceF[Either[DeprecatedRealm, ActiveRealm]]
+  type OptResource = Option[Resource]
+
+  /**
+    * The constant collection of realm types.
+    */
+  final val types: Set[AbsoluteIri] = Set(nxv.Realm.value)
+
+  /* Constant permissions */
+  final val read  = Permission.unsafe("realms/read")
+  final val write = Permission.unsafe("realms/write")
+
+}
