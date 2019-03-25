a32b9024eeb3d8fb8829ad0bd80e929a4a72e649
Henry <henry.genet@epfl.ch>
2019-03-25 12:02:13 +0100

    Fix Permission regex

--- src/main/scala/ch/epfl/bluebrain/nexus/iam/types/Permission.scala
+++ src/main/scala/ch/epfl/bluebrain/nexus/iam/types/Permission.scala
@@ -4,22 +4,26 @@ import cats.Show
 import io.circe._
 
 /**
-  * Wraps a permission string, e.g. ''own'', ''read'', ''write''.
+  * Wraps a permission string that must begin with a letter, followed by at most 31
+  * alphanumeric characters or symbols among '-', '_', ':', '\' and '/'.
   *
   * @param value a valid permission string
   */
 final case class Permission private (value: String)
 
 object Permission {
-  private val valid = "^[a-zA-Z-:_\\/]{1,32}$".r
+
+  private val valid = """[a-zA-Z][\w-:\\/]{0,31}""".r
 
   /**
     * Attempts to construct a [[Permission]] that passes the ''regex''
     *
     * @param value the permission value
     */
-  final def apply(value: String): Option[Permission] =
-    valid.findFirstIn(value).map(unsafe)
+  final def apply(value: String): Option[Permission] = value match {
+    case valid() => Some(unsafe(value))
+    case _       => None
+  }
 
   /**
     * Constructs a [[Permission]] without validating it against the ''regex''
